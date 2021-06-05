70363e48582cc8b0a21afef8319c5ce128b6d436
renvelvet <reshapuspita734@gmail.com>
2021-06-05 20:22:54 +0700

    init commit

---
+++ app/src/main/java/com/dicoding/skinai/ui/RegisActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.skinai.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regis)
+    }
+}

1c3c9cd111e78d782fa68fdfbb4b3e33ab2b4edc
renvelvet <reshapuspita734@gmail.com>
2021-06-09 23:08:18 +0700

    fix LoginActivity.kt prototype

--- app/src/main/java/com/dicoding/skinai/ui/LoginActivity.kt
+++ app/src/main/java/com/dicoding/skinai/ui/LoginActivity.kt
@@ -1,12 +1,34 @@
 package com.dicoding.skinai.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.skinai.R
+import com.dicoding.skinai.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbar.title = getString(R.string.login)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.elevation = 0f
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ion_return)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.buttonLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.tvSignUp.setOnClickListener {
+            val intent = Intent(this, RegisActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/dicoding/skinai/ui/RegisActivity.kt
+++ app/src/main/java/com/dicoding/skinai/ui/RegisActivity.kt
@@ -9,17 +9,24 @@ import com.dicoding.skinai.databinding.ActivityRegisBinding
 class RegisActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.toolbar.title = getString(R.string.sign_up)
         setSupportActionBar(binding.toolbar)
         supportActionBar?.elevation = 0f
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ion_return)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         binding.buttonSign.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.tvLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
